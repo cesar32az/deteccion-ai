f170bb83b7923c83af455e460c165b8571a36c22
Julio R <cesar32az@gmail.com>
2021-05-26 21:38:45 -0600

    probando la carga de imagenes

--- backend-ai/core.py
+++ backend-ai/core.py
@@ -1,3 +1,15 @@
+import cv2
+import numpy as np
+import pathlib
+from pathlib import Path
+
+
 def deteccion(file):
+    #dir = pathlib.Path().parent.absolute()
+    dir = '/home/julio/umg/inteligencia-artificial/proyecto-final-ai/backend-ai/upload'
+    img_path = f'{dir}/{file.filename}'
+    print(img_path)
+
     print(file.filename)
     return 'Gato'
+    
